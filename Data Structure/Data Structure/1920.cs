b5fba9372038f6a723642d61f65c16ed3b269bc5
Quartzs_Book <progquartz@gmail.com>
2022-08-14 18:50:55 +0900

    Week3. Binary Search - 이진 탐색 관련 문제 풀이 (1)

---
+++ Data Structure/Data Structure/1920.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure
+{
+    class _1920
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int.Parse(Console.ReadLine());
+            //이미 정렬되어있는 int 배열
+            int[] datas = new int[] { 1, 2, 3, 5, 12, 13, 31, 53, 56 };
+
+            int result = BinarySearch(datas, 1);
+
+            View(result);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 이진 탐색을 수행한다.
+        /// </summary>
+        /// <param name="datas">배열</param>
+        /// <param name="data">검색할 데이터</param>
+        /// <returns></returns>
+        private static int BinarySearch(int[] datas, int data)
+        {
+            int right = datas.Length - 1;
+            for (int left = 0; left <= right;)
+            {
+                int middle = (left + right) / 2;
+                switch (Compare(data, datas[middle]))
+                {
+                    case '>': left = middle + 1; break;//x>datas[middle] 
+                    case '<': right = middle - 1; break;//x<datas[middle]
+                    case '=': return middle; //x==datas[middle]
+                }
+
+            }
+            //발견되지 않음
+            return -1;
+        }
+
+        /// <summary>
+        /// x와 y를 비교한다.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private static char Compare(int x, int y)
+        {
+            if (x > y)
+                return '>';
+            else if (x < y)
+                return '<';
+            else
+                return '=';
+        }
+
+
+
+        /// <summary>
+        /// 화면에 출력한다.
+        /// </summary>
+        /// <param name="result"></param>
+        private static void View(int result)
+        {
+            Console.Write("{0} ", result);
+        }
+    }
+}
